cef8512f98f94025e6b6210353b0fa35609f8e42
Gonzalo Baez <gonzaasd7@gmail.com>
2025-12-05 18:03:51 -0300

    crud institucion y docente

---
+++ index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const server = require("./src/server");
+const { conn } = require("./src/db.js");
+const PORT = process.env.PORT || 3000;
+
+async function startServer() {
+    try {
+        console.log("🔌 Autenticando DB...");
+        await conn.authenticate();
+        console.log("✅ DB autenticada.");
+
+        console.log("🧱 Sincronizando modelos...");
+        await conn.sync({ force: false, alter: false });
+        console.log("✅ Modelos sincronizados.");
+
+        console.log("🌱 Insertando datos iniciales...");
+        console.log("✅ Datos iniciales insertados.");
+
+        console.log(`🚀 Iniciando servidor en puerto ${PORT}`);
+        server.listen(PORT, '0.0.0.0', () => {
+            console.log(`✅ Servidor corriendo en puerto ${PORT}`);
+        });
+
+    } catch (error) {
+        console.error("🔥 Error en el arranque:", error.message);
+    }
+}
+
+startServer();
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
-const docenteController = require("../controllers/docente");
+const docenteCrudService = require("../services/docente/docenteCrudService")
+const institucionCrudService = require("../services/institucion/institucionCrudService")
+const docenteController = require("../controllers/docente")(docenteCrudService);
 const docenteRouter = require("../routes/docente");
 const institutionRouter = require("./institucion");
-const institutionController = require("../controllers/institucion");
+const institutionController = require("../controllers/institucion")(institucionCrudService);
+const { Router } = require("express")
 
 const app = express();
 app.use(express.json());
+const router = Router();
 
-app.use("/docentes", docenteRouter(docenteController));
-app.use("/institucion", institutionRouter(institutionController))
+console.log("INSTITUTION ROUTER:", institutionRouter);
+console.log("DOCENTE ROUTER:", docenteRouter);
+
+
+router.use("/docentes", docenteRouter(docenteController));
+router.use("/institucion", institutionRouter(institutionController))
+
+
+module.exports = router
